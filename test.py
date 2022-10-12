b1e1e401ef9d4b87374106f0347cf2c565e718c4
wStar-10 <102585065+wStar-10@users.noreply.github.com>
2022-10-12 15:54:53 +0800

    Add files via upload
    修复bug：增添修改请求头部的方法

--- test.py
+++ test.py
@@ -7,9 +7,9 @@ import json
 # 请求头
 my_header = {
     "User-Agent":"User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36 MicroMessenger/7.0.9.501 NetType/WIFI MiniProgramEnv/Windows WindowsWechat",
-    "imprint":"oWRkU0X0y2TYFRDqFFdGW153oLM0",
     "Referer":"https://servicewechat.com/wx23d8d7ea22039466/1674/page-frame.html",
 }
+# "imprint": "oWRkU0X0y2TYFRDqFFdGW153oLM0",
 # "Accept-Encoding": "gzip, deflate, br",
 # "Connection": " keep-alive",
 # "Host": "a.welife001.com"
@@ -26,9 +26,6 @@ filename = 'data.xlsx'
 sheet_name = 'Sheet1'
 filename_data = ''
 file_log = 'log.txt'
-# dict_data = {"extra":1,"id":"632886752186540a0fb177df","daka_day":"2022-10-09","submit_type":"submit","networkType":"wifi","member_id":"63292a61a6ea452aaf90f859","op":"add","invest":{"is_tmp":False,"is_private":False,"_id":"6328867577181f99d2d26fc9","subject":[{"seq":0,"cate":1,"inputs_count":0,"inputs":[],"item_details":[{"seq":0,"checks_count":0,"rate":0,"file":[],"checkedlist":[],"_id":"6328867577181f99d2d26fcb","name":"是","checked":True},{"seq":1,"checks_count":0,"rate":0,"file":[],"checkedlist":[],"_id":"6328867577181f99d2d26fcc","name":"否","checked":False}],"_id":"6328867577181f99d2d26fca","title":"身体是否健康","required":True,"valid":True},{"seq":1,"cate":2,"inputs_count":0,"inputs":[],"item_details":[{"seq":0,"checks_count":0,"rate":0,"file":[],"checkedlist":[],"_id":"6328867577181f99d2d26fce","name":""},{"seq":1,"checks_count":0,"rate":0,"file":[],"checkedlist":[],"_id":"6328867577181f99d2d26fcf","name":""}],"_id":"6328867577181f99d2d26fcd","title":"体温（早 中 晚）","required":True,"input":{"content":"36.5 36.6 36.6","file":[]},"valid":True},{"seq":2,"cate":4,"inputs_count":0,"inputs":[],"item_details":[{"seq":0,"checks_count":0,"rate":0,"file":[],"checkedlist":[],"_id":"6328867577181f99d2d26fd1","name":""},{"seq":1,"checks_count":0,"rate":0,"file":[],"checkedlist":[],"_id":"6328867577181f99d2d26fd2","name":""}],"_id":"6328867577181f99d2d26fd0","title":"手机定位","required":True,"input":{"content":"{\"title\":\"大连市甘井子区人民政府\",\"address\":\"辽宁省大连市甘井子区明珠广场1号\",\"location\":[121.525529,38.953054]}"},"valid":True},{"seq":3,"cate":1,"inputs_count":0,"inputs":[],"item_details":[{"seq":0,"checks_count":0,"rate":0,"file":[],"checkedlist":[],"_id":"6328867577181f99d2d26fd4","name":"是","checked":False},{"seq":1,"checks_count":0,"rate":0,"file":[],"checkedlist":[],"_id":"6328867577181f99d2d26fd5","name":"否","checked":True}],"_id":"6328867577181f99d2d26fd3","title":"是否处于隔离状态","required":True,"valid":True},{"seq":4,"cate":1,"inputs_count":0,"inputs":[],"item_details":[{"seq":0,"checks_count":0,"rate":0,"file":[],"checkedlist":[],"_id":"6328867577181f99d2d26fd7","name":"常态化","checked":True},{"seq":1,"checks_count":0,"rate":0,"file":[],"checkedlist":[],"_id":"6328867577181f99d2d26fd8","name":"低风险","checked":False},{"seq":2,"checks_count":0,"rate":0,"file":[],"checkedlist":[],"_id":"6328867577181f99d2d26fd9","name":"中风险","checked":False},{"seq":3,"checks_count":0,"rate":0,"file":[],"checkedlist":[],"_id":"6328867577181f99d2d26fda","name":"高风险","checked":False}],"_id":"6328867577181f99d2d26fd6","title":"目前所在地区风险等级（国务院小程序查询）","required":True,"valid":True},{"seq":5,"cate":2,"inputs_count":0,"inputs":[],"item_details":[{"seq":0,"checks_count":0,"rate":0,"file":[],"checkedlist":[],"_id":"6328867577181f99d2d26fdc","name":""},{"seq":1,"checks_count":0,"rate":0,"file":[],"checkedlist":[],"_id":"6328867577181f99d2d26fdd","name":""}],"_id":"6328867577181f99d2d26fdb","title":"是否有需要指导员协助事宜","required":False,"valid":True,"input":{"content":"否","file":[]}}],"create_at":"2022-09-19T15:10:45.068Z","update_at":"2022-09-19T15:10:45.068Z","__v":0,"time":1665293821680,"day":"2022-10-09"},"feedback_text":""}
-#
-# dict_data_or = {"extra":1,"id":"632886752186540a0fb177df","daka_day":"2022-10-09","submit_type":"submit","networkType":"wifi","member_id":"63292a61a6ea452aaf90f859","op":"add","invest":{"is_tmp":False,"is_private":False,"_id":"6328867577181f99d2d26fc9","subject":[{"seq":0,"cate":1,"inputs_count":0,"inputs":[],"item_details":[{"seq":0,"checks_count":0,"rate":0,"file":[],"checkedlist":[],"_id":"6328867577181f99d2d26fcb","name":"是","checked":True},{"seq":1,"checks_count":0,"rate":0,"file":[],"checkedlist":[],"_id":"6328867577181f99d2d26fcc","name":"否","checked":False}],"_id":"6328867577181f99d2d26fca","title":"身体是否健康","required":True,"valid":True},{"seq":1,"cate":2,"inputs_count":0,"inputs":[],"item_details":[{"seq":0,"checks_count":0,"rate":0,"file":[],"checkedlist":[],"_id":"6328867577181f99d2d26fce","name":""},{"seq":1,"checks_count":0,"rate":0,"file":[],"checkedlist":[],"_id":"6328867577181f99d2d26fcf","name":""}],"_id":"6328867577181f99d2d26fcd","title":"体温（早 中 晚）","required":True,"input":{"content":"36.5 36.6 36.6","file":[]},"valid":True},{"seq":2,"cate":4,"inputs_count":0,"inputs":[],"item_details":[{"seq":0,"checks_count":0,"rate":0,"file":[],"checkedlist":[],"_id":"6328867577181f99d2d26fd1","name":""},{"seq":1,"checks_count":0,"rate":0,"file":[],"checkedlist":[],"_id":"6328867577181f99d2d26fd2","name":""}],"_id":"6328867577181f99d2d26fd0","title":"手机定位","required":True,"input":{"content":"{\"title\":\"大连市甘井子区人民政府\",\"address\":\"辽宁省大连市甘井子区明珠广场1号\",\"location\":[121.525529,38.953054]}"},"valid":True},{"seq":3,"cate":1,"inputs_count":0,"inputs":[],"item_details":[{"seq":0,"checks_count":0,"rate":0,"file":[],"checkedlist":[],"_id":"6328867577181f99d2d26fd4","name":"是","checked":False},{"seq":1,"checks_count":0,"rate":0,"file":[],"checkedlist":[],"_id":"6328867577181f99d2d26fd5","name":"否","checked":True}],"_id":"6328867577181f99d2d26fd3","title":"是否处于隔离状态","required":True,"valid":True},{"seq":4,"cate":1,"inputs_count":0,"inputs":[],"item_details":[{"seq":0,"checks_count":0,"rate":0,"file":[],"checkedlist":[],"_id":"6328867577181f99d2d26fd7","name":"常态化","checked":True},{"seq":1,"checks_count":0,"rate":0,"file":[],"checkedlist":[],"_id":"6328867577181f99d2d26fd8","name":"低风险","checked":False},{"seq":2,"checks_count":0,"rate":0,"file":[],"checkedlist":[],"_id":"6328867577181f99d2d26fd9","name":"中风险","checked":False},{"seq":3,"checks_count":0,"rate":0,"file":[],"checkedlist":[],"_id":"6328867577181f99d2d26fda","name":"高风险","checked":False}],"_id":"6328867577181f99d2d26fd6","title":"目前所在地区风险等级（国务院小程序查询）","required":True,"valid":True},{"seq":5,"cate":2,"inputs_count":0,"inputs":[],"item_details":[{"seq":0,"checks_count":0,"rate":0,"file":[],"checkedlist":[],"_id":"6328867577181f99d2d26fdc","name":""},{"seq":1,"checks_count":0,"rate":0,"file":[],"checkedlist":[],"_id":"6328867577181f99d2d26fdd","name":""}],"_id":"6328867577181f99d2d26fdb","title":"是否有需要指导员协助事宜","required":False,"valid":True,"input":{"content":"否","file":[]}}],"create_at":"2022-09-19T15:10:45.068Z","update_at":"2022-09-19T15:10:45.068Z","__v":0,"time":1665293821680,"day":"2022-10-09"},"feedback_text":""}
 
 # 日志输出方式
 # with open("text.txt","a") as file:
@@ -61,7 +58,9 @@ def read_request_body(filename):
 def write_log(file_log,content):
     with open(file_log,"a") as file:
         file.write(content)
-
+# 修改请求头
+def modify_header(data_person):
+    my_header['imprint'] = data_person[12]
 # 修改信息
 def modify_data(data_person):
     # 问卷id
@@ -121,6 +120,7 @@ if __name__ == '__main__':
             dict_data = eval(read_request_body(filename_data))
             modify_data(data_temp)
             url =host+url_submit
+            modify_header(data_temp)
             send_request(url,dict_data,str(data_temp[10]))
         print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
         print('填报成功，等待八小时后再次填报......')
